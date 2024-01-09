fd4cd4f5b20a37aba1198b1e4a8942c4fc47a197
leeedohyun <ldh19738@gmail.com>
2024-01-09 13:49:27 +0900

    refactor: 테스트 메서드를 직접 호출하는 대신 인터페이스의 메서드를 호출하도록 리팩토링

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -9,4 +9,8 @@ public class WasRun {
     public void testMethod() {
         wasRun = true;
     }
+
+    public void run() {
+        testMethod();
+    }
 }
--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -4,7 +4,7 @@ public class XUnitTest {
     public static void main(String[] args) {
         WasRun test = new WasRun("testMethod");
         System.out.println(test.wasRun); // false
-        test.testMethod();
+        test.run();
         System.out.println(test.wasRun); // true
     }
 }
