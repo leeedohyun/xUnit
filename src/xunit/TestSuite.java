80c7f26d05df5604fd9b6074e2878c15e5cb39b0
leeedohyun <ldh19738@gmail.com>
2024-01-09 16:34:47 +0900

    feat: 테스트 자동화

--- src/xunit/TestSuite.java
+++ src/xunit/TestSuite.java
@@ -1,11 +1,28 @@
 package xunit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestSuite implements Test {
 
     private List<Test> tests = new ArrayList<>();
+
+    public TestSuite() {
+    }
+
+    public TestSuite(final Class<? extends TestCase> testClass) {
+        Arrays.stream(testClass.getDeclaredMethods())
+                .filter(m -> m.getName().startsWith("test"))
+                .forEach(m -> {
+                            try {
+                                add(testClass.getConstructor(String.class).newInstance(m.getName()));
+                            } catch (final Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+    }
 
     public void add(final Test test) {
         tests.add(test);
