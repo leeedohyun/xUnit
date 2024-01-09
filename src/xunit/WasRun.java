1a2af59921d99cfe9a36ab2b8d71b40254b85b1f
leeedohyun <ldh19738@gmail.com>
2024-01-09 13:44:32 +0900

    test: 성공하는 테스트 코드 작성

---
+++ src/xunit/WasRun.java
@@ -0,0 +1,12 @@
+package xunit;
+
+public class WasRun {
+    public boolean wasRun;
+
+    public WasRun(final String name) {
+    }
+
+    public void testMethod() {
+        wasRun = true;
+    }
+}
